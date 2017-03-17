c6007695df4920471c311b84a6b52298826fa443
Alex Lee <doomcat9167@gmail.com>
2017-03-17 08:52:30 -0700

    add base Event object, add getters and setters, and implement tests for all the above

---
+++ src/main/java/Event.java
@@ -0,0 +1,80 @@
+
+public class Event{
+  private int mNumOfGuests = 0;
+  private String mFoodOption;
+  private String mBeverageOption;
+  private String mEntertainmentOption;
+
+  public Event(int guests, String food, String bev, String ent){
+    mNumOfGuests = guests;
+    mFoodOption = food;
+    mBeverageOption = bev;
+    mEntertainmentOption = ent;
+  }
+
+
+  public int calculateCost(){
+    int cost = 0;
+
+    //guest numbers cost
+    if (mNumOfGuests < 50){cost += 50;}
+    else if(mNumOfGuests < 100 && mNumOfGuests >= 50){cost += 100;}
+    else if(mNumOfGuests >= 100){cost += 200;}
+    else{System.out.println("Please enter a valid number");}
+
+    //food option cost
+    if (mFoodOption == "1"){cost += 50;}
+    else if(mFoodOption == "2"){cost += 250;}
+    else if (mFoodOption == "3"){cost += 500;}
+    else{System.out.println("Please enter a valid option");}
+
+    //drink option cost
+    if (mBeverageOption == "1"){cost += 0;}
+    else if(mBeverageOption == "2"){cost += 100;}
+    else if(mBeverageOption == "3"){cost += 300;}
+    else {System.out.println("Please enter a valid option");}
+
+    //Entertainment option cost
+    if (mEntertainmentOption == "1"){cost +=0;}
+    else if (mEntertainmentOption == "2"){cost +=300;}
+    else if (mEntertainmentOption == "3"){cost += 500;}
+    else {System.out.println("Please enter a valid option");}
+
+    return cost;
+  }
+
+
+
+  //Getters
+  public int getNumOfGuests(){
+    return mNumOfGuests;
+  }
+
+  public String getFoodOption(){
+    return mFoodOption;
+  }
+  public String getBeverageOption(){
+    return mBeverageOption;
+  }
+  public String getEntertainmentOption(){
+    return mEntertainmentOption;
+  }
+
+  //Setters
+  public void setNumOfGuests(int num){
+    mNumOfGuests = num;
+  }
+  public void setFoodOption(String input){
+    mFoodOption = input;
+  }
+  public void setBeverageOption(String input){
+    mBeverageOption = input;
+  }
+  public void setEntertainmentOption(String input){
+    mEntertainmentOption = input;
+  }
+
+  public String toString(){
+    return "Number of guests: " + mNumOfGuests + ", food option: " + "mFoodOption" + mFoodOption + ", beverage option: " + mBeverageOption + ", entertainment option: " + mEntertainmentOption;
+  }
+}
---
+++ src/test/java/test.java
@@ -0,0 +1,61 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class test{
+
+  @Test
+  public void createEventObject(){
+    Event event = new Event(50, "1a", "2b", "3b");
+    assertEquals(true, event instanceof Event);
+  }
+
+  @Test
+  public void testGetters(){
+    Event event = new Event(50, "1a", "2b", "3b");
+
+    int expectedA = 50;
+    String expectedB = "1a";
+    String expectedC = "2b";
+    String expectedD = "3b";
+
+    assertEquals(expectedA, event.getNumOfGuests());
+    assertEquals(expectedB, event.getFoodOption());
+    assertEquals(expectedC, event.getBeverageOption());
+    assertEquals(expectedD, event.getEntertainmentOption());
+
+  }
+  @Test
+  public void testSetters(){
+    Event event = new Event(0, "", "", "");
+
+    event.setNumOfGuests(50);
+    event.setFoodOption("1a");
+    event.setBeverageOption("2b");
+    event.setEntertainmentOption("3b");
+
+    int expectedA = 50;
+    String expectedB = "1a";
+    String expectedC = "2b";
+    String expectedD = "3b";
+
+    assertEquals(expectedA, event.getNumOfGuests());
+    assertEquals(expectedB, event.getFoodOption());
+    assertEquals(expectedC, event.getBeverageOption());
+    assertEquals(expectedD, event.getEntertainmentOption());
+
+  }
+
+  @Test
+  public void calculateCosts(){
+    Event event = new Event(0 ,"", "", "");
+
+    event.setNumOfGuests(120);
+    event.setFoodOption("2");
+    // event.setNumOfGuests(120);
+    // event.setNumOfGuests(120);
+    int expectedTotal = 450;
+
+    assertEquals(expectedTotal, event.calculateCost());
+
+  }
+}
